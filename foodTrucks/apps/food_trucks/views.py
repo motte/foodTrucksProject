27c54888351171505dc5ab47f3d84f23a90a03bd
motte <michaelotte1@gmail.com>
2015-03-26 01:54:39 -0700

    [MO] Get SF food truck data from Socratic
    https://data.sfgov.org/Economy-and-Community/Mobile-Food-Facility-Permit
    /rqzj-sfat?

--- foodTrucks/apps/food_trucks/views.py
+++ foodTrucks/apps/food_trucks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
+import urllib
+import json
 
 
 class FoodTruckTemplateView(TemplateView):
@@ -7,6 +9,20 @@ class FoodTruckTemplateView(TemplateView):
 
     def dispatch(self, request, *args, **kwargs):
         return super(FoodTruckTemplateView, self).dispatch(request, *args, **kwargs)
+
+    @staticmethod
+    def associate_yelp(input_data):
+        output_data = input_data
+        for truck in input_data:
+            print truck
+        return output_data
+
+    def get_context_data(self, **kwargs):
+        response_data = urllib.urlopen('https://data.sfgov.org/resource/rqzj-sfat.json')
+        json_data = json.loads(response_data.read())
+        context = super(FoodTruckTemplateView, self).get_context_data(**kwargs)
+        context['food_truck_data'] = self.associate_yelp(json_data)
+        return context
 
 
 class FoodTruckByCityListView(TemplateView):
